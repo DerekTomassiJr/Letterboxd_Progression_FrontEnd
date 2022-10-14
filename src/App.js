7cad8a605b6706022924e941ea525c1a5bb8de5d
derektomassijr <derektomassi@me.com>
2022-10-14 14:08:00 -0400

    Completed Navbar Design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Navbar from "./components/Navbar.js";
 import Home from "./pages/Home.js";
-import About from "./pages/About.js";
-import Pricing from "./pages/Pricing.js";
+import Directors from "./pages/Directors.js";
+import Profile from "./pages/Profile.js";
+import Actors from "./pages/Actors.js";
+import Achievements from "./pages/Achievements.js";
+import Settings from "./pages/Settings.js";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
@@ -13,8 +16,11 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/directors" element={<Directors />} />
+          <Route path="/actors" element={<Actors />} />
+          <Route path="/achievements" element={<Achievements />} />
+          <Route path="/settings" element={<Settings />} />
         </Routes>
       </div>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import letterboxdLogo from "C:/Users/derek/Documents/Code/Web Devlopement/My Work/Letterboxd_Progression_Website/letterboxd-progression/src/images/letterboxd_logo.png";
+import settingsIcon from "C:/Users/derek/Documents/Code/Web Devlopement/My Work/Letterboxd_Progression_Website/letterboxd-progression/src/images/settings_icon.png";
+import profilePicture from "C:/Users/derek/Documents/Code/Web Devlopement/My Work/Letterboxd_Progression_Website/letterboxd-progression/src/images/profile_picture.JPG"
 
 export default function Navbar() {
     const path = window.location.pathname;
     return ( 
     <nav className="nav">
-        <Link to="/" className="site-title">Site Name</Link>
+        <Link to="/" className="site-title"><img id="site_logo" src={ letterboxdLogo } /></Link>
         <ul>
-            <CustomLink to="/pricing">Pricing</CustomLink>
-            <CustomLink to="/about">About</CustomLink>
+            <CustomLink to="/profile"><img id="profile_picture" src={ profilePicture } />Profile</CustomLink>
+            <CustomLink to="/directors">Directors</CustomLink>
+            <CustomLink to="/actors">Actors</CustomLink>
+            <CustomLink to="/achievements">Achievements</CustomLink>
+            <CustomLink to="/settings"><img id="settings_icon" src={ settingsIcon } /></CustomLink>
         </ul>
     </nav>
     )
